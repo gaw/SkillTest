1b783ab3df1f5a58ecf5205b996c5ea3d94c92ca
Rolemancer <newsomebox@gmail.com>
2013-09-06 12:59:09 +0400

    Multiplayer. Test 2.

---
+++ Assets/Scripts/NetAction1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetAction1 : MonoBehaviour 
+{
+	void Update () 
+	{
+		if (Network.isServer)
+		{
+		    Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		    float speed = 5;
+		    transform.Translate(speed * moveDir * Time.deltaTime);
+		}
+	}
+}
