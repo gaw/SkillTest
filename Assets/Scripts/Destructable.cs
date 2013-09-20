75524df58f3e4f032ba5218f9aad4a2ab79ce323
Rolemancer <newsomebox@gmail.com>
2013-09-20 19:05:55 +0400

    Destroyable building

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructable : MonoBehaviour 
+{
+	public GameObject destructableObj;
+	
+	void OnMouseDown()
+	{
+		DestoyMe();
+	}
+	
+	private void DestoyMe()
+	{
+		Instantiate(destructableObj, transform.position, transform.rotation);
+		Destroy(gameObject);	
+	}
+	
+}
